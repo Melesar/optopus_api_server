d3c50e9098147d8daf4b49893493432b2a52cb3b
Хозяин <>
2017-02-03 14:32:01 +0200

    Fixed {0} name_format

--- models/Bundle.php
+++ models/Bundle.php
@@ -63,31 +63,18 @@ class Bundle extends ActiveRecord
     {
         $path = Yii::getAlias('@web').$requestData['bundle_size'].'@'.$requestData['name_format'];
 
-        if(is_dir($path))
-        {
+        if(is_dir($path)) {
             $zip = new \ZipArchive();
-            $zip->open($path.'.zip', \ZipArchive::CREATE);
+            $zip->open($path . '.zip', \ZipArchive::CREATE);
             $zip->addEmptyDir($path);
 
-            /**
-             * Probably should use the same pattern as in findAndSend
-             * but inside the for loop. So you will manage to get all needed files
-             * right on the fly.
-             */
-            $name_format = preg_replace('/[^_]+$/s', '', $requestData['name_format']);
+            $name_format = preg_replace('/\{.*/', '', $requestData['name_format']);
 
             for($i = 1; $i <= $requestData['bundle_size']; $i++)
-            {
-                /**
-                 * Use the constant variable instead of .txt
-                 */
                 $zip->addFile($path.'/'.$name_format.$i.self::FILE_EXTENSION);
-            }
+
             $zip->close();
 
-            /**
-             * We need to set response headers, so changed request to response
-             */
             $header = Yii::$app->response->headers;
             $header->add('Content-Type','application/zip');
             $header->add('Content-Disposition','attachment; filename='.basename($path.'.zip'));
@@ -105,29 +92,15 @@ class Bundle extends ActiveRecord
     {
         $path = Yii::getAlias('@web').$requestData['bundle_size'].'@'.$requestData['name_format'];
         $pathFile = '/'.$requestData['name_format'];
-        /**
-         * Provided pattern would replace only number. So location_{0} will be transformed into
-         * location_{1} which is incorrect. Should also replace curly brackets
-         */
-        $pathFile = preg_replace('/\d{1,}/',$requestData['number'],$pathFile);
+        $pathFile = preg_replace('/\{0\}/',$requestData['number'],$pathFile);
         $path .= $pathFile.self::FILE_EXTENSION;
         if(file_exists($path))
         {
-            /**
-             * The same, set response headers via Yii::$app->response
-             * Content-Type: text/* is incorrect since we send byte data
-             * Content-Length should be the length of the byte array rather then filesize
-             */
-            //if you send *.txt file, server writes at the end of it the amount of symbols
-            header('Content-Type: text/*');
-            header('Content-Disposition: attachment; filename=' . basename($path));
-            header('Content-Length: ' . filesize($path));
+            $header = Yii::$app->response->headers;
+            $header->add('Content-Type','application/octet-stream');
+            $header->add('Content-Disposition','attachment; filename='.basename($path));
+            $header->add('Content-Length',filesize($path));
 
-            /**
-             * readfile is inappropriate here. This function is used to display files onto the screen
-             * and it returns only a number of bytes read. Should change to something that returns the actual
-             * byte array
-             */
             $byte = readfile($path);
             return $byte;
         }
