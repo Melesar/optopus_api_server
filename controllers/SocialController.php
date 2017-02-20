9f7f78f2255279dfd58a093c0f9481e83458dc24
Oleksii Shabalin <shabalin_alex_1996@mail.ru>
2017-02-20 18:47:23 +0200

    GET User

--- controllers/SocialController.php
+++ controllers/SocialController.php
@@ -55,50 +55,7 @@ class SocialController extends Controller
     {
         $SAC = Yii::$app->request->get('SAC');
         $app_user = AppUser::findOne(['SAC' => $SAC]);
-        $user = Users::findOne(['id' => $app_user['USER_ID']]);
-        $booster_array = $user->getBooster();
-       // Users::find()->join('LEFT JOIN', 'USER_BOOSTER', 'user_booster.user_id = users.id');
-        $q1 = (new \yii\db\Query())
-            ->select('*')
-            ->from('users')
-            ->where(['id' => $app_user['USER_ID']]);
 
-        $q2 = (new \yii\db\Query())
-            ->select('*')
-            ->from('user_booster')
-            ->where(['USER_ID' => $app_user['USER_ID']]);
-
-        $q3 = $q1->innerJoin('user_booster','user_booster.USER_ID = users.id');
-        $q5 = $q3->innerJoin('booster','booster.ID = BOOSTER_ID');
-
-        $q4 = $q5->all();
-
-        return $q4;
-
-        //$res = Users::find()->innerJoinWith('booster')->all();
-        //$res = UserBooster::find()->innerJoinWith('users')->where(['id'=>$user['id']])->all();
-
-        // workable
-        // SELECT id, user_booster.booster_id
-        // FROM users
-        // INNER JOIN user_booster
-        // ON user_booster.USER_ID = users.id
-        // WHERE user_booster.user_id = 263711717395626;
-
-//        $answ = Users::find()
-//        ->select('id, user_booster.booster_id')
-//            ->from('users')
-//            ->innerJoin('user_booster', '`user_booster`.`USER_ID` = `users`.`id`')
-//            ->where(['user_booster.user_id' => $user['id']])
-//            ->with('userBooster')
-//            ->all();
-//        $res2 = Users::find()
-//            ->where(['id'=>$user['id']])
-//            ->with(['userBooster','userBooster.booster'])
-//            ->all();
-
-
-
-
+        return Social::getUserMultitableData($app_user);
     }
 }
--- models/Social.php
+++ models/Social.php
@@ -13,8 +13,26 @@ use yii\db\ActiveRecord;
 
 class Social extends ActiveRecord
 {
-    public function dataRefresh()
+    public static function getUserMultitableData($app_user)
     {
+        $q1 = (new \yii\db\Query())
+            ->select('*')
+            ->from('user_booster');
 
+        $q2 = $q1->innerJoin('users','user_booster.USER_ID = users.id')
+            ->innerJoin('booster','booster.ID = user_booster.BOOSTER_ID')
+            ->innerJoin('app_user','user_booster.USER_ID = app_user.USER_ID');
+
+        $q3 = $q2
+            ->select('users.id AS USER_ID, users.name AS FIRST_NAME,
+            users.last_name AS LAST_NAME, app_user.MONEY,
+            users.avatar_url AS PICTURE, users.first_authorized AS SIGNED_UP,
+            users.last_online AS LAST_ONLINE, booster.ID AS BOOSTER_ID,
+            booster.ALIAS, booster.NAME AS BOOSTER_NAME,
+            booster.DESCRIPTION, booster.COST')
+            ->where(['users.id' => $app_user['USER_ID']])
+            ->all();
+
+        return $q3;
     }
 }
